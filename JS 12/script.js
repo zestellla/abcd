d1d6a4402d7992418db132d1e603e1e87dce7259
zestellla <rantiaisyy02@gmail.com>
2024-10-11 09:55:46 +0700

    commit pertama

---
+++ JS 12/script.js
@@ -0,0 +1,15 @@
+// var a = 10;
+// var b = 23;
+
+// alert(a+b);
+// console,log(a*b);
+// document.write(b-a);
+
+//var namaInput = prompt('masukan nama kamu')
+//alert(`welcome ${namaInput} di website kami`);
+
+let text = ' ';
+for (let i = 1; i <= 3; i++){
+   text = `${i*2} ` ;
+   document.write(text);
+}
